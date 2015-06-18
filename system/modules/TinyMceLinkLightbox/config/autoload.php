971e6681a4d06daec341164e3010f2c2516843d1
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-06-18 10:42:02 +0200

    Initial

---
+++ system/modules/TinyMceLinkLightbox/config/autoload.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package TinyMceLinkLightbox
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+	'TinyMceLinkLightbox',
+));
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Classes
+	'TinyMceLinkLightbox\TinyMceLinkLightbox' => 'system/modules/TinyMceLinkLightbox/classes/TinyMceLinkLightbox.php',
+));
